e83ab5c744ebade629081f78cee7e67f5cac82c0
nikhilsharma8997 <nikhil.sharma8398@gmail.com>
2019-02-17 23:03:10 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
 /*!
-    I created this JavaScript file in order to get the webpage a little more functional.
-    I have added some animations as well which you might have noticed while
-    navigating to different sections of the web page using the navigation
-    bar present at the top of the page.
+    This is the JavaScript file is associated with index.html
 
     Description: This file contains all the scripts associated with the web page.
     portfolio website.
 */
 
-(function($) {
+(function ($) {
 
     // Remove no-js class
     $('html').removeClass('no-js');
 
     // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    $('header a').click(function (e) {
 
         // Treat as normal link if no-scroll class
         if ($(this).hasClass('no-scroll')) return;
 
-        e.preventDefault();
         var heading = $(this).attr('href');
         var scrollDistance = $(heading).offset().top;
 
@@ -34,14 +30,14 @@
     });
 
     // Scroll to top
-    $('#to-top').click(function() {
+    $('#to-top').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
     });
 
     // Scroll to first element
-    $('#lead-down span').click(function() {
+    $('#lead-down span').click(function () {
         var scrollDistance = $('#lead').next().offset().top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
@@ -49,71 +45,49 @@
     });
 
     // Open mobile menu
-    $('#mobile-menu-open').click(function() {
+    $('#mobile-menu-open').click(function () {
         $('header, body').addClass('active');
     });
 
     // Close mobile menu
-    $('#mobile-menu-close').click(function() {
+    $('#mobile-menu-close').click(function () {
         $('header, body').removeClass('active');
     });
 
     // Load additional projects
-    $('#view-more-projects').click(function(e){
+    $('#view-more-projects').click(function (e) {
         e.preventDefault();
-        $(this).fadeOut(300, function() {
+        $(this).fadeOut(300, function () {
             $('#more-projects').fadeIn(300);
         });
     });
-    
+
 })(jQuery);
 
-function toggleAddModal() {
-    var check = document.getElementById("modalDefaultContactForm");
-    console.log("Button click: " + check.style.display);
-    if (check.style.display == "") {
-        console.log("setting block");
-        check.style.display = "block";
-    } 
-    else {
-        check.style.display = "";
-        console.log("setting none");
-    }
+getJSONObject = function () {
+    return JSON.parse(localStorage.getItem('imagesLocalStorage'));
 }
-function toggleEditModal() {
-    var check = document.getElementById("editModal");
-    if (check.style.display == "none") {
-        check.style.display = "block";
-    }
-    else {
-        check.style.display = "none";
-    }
-}
-
-var galleryObject, output;
-$.getJSON("galleryImages.json", function(data){
-    galleryObject = data;
-    galleryObject.images.forEach(function(obj) {
-        addNewModel(obj.imageUrl, obj.youtubeLink);
+var localStorageJSON = getJSONObject();
+console.log(localStorageJSON);
+if (localStorageJSON == null) {
+    $.getJSON("galleryImages.json", function (data) {
+        galleryObject = data;
+        galleryObject.images.forEach(function (obj) {
+            addNewImage(obj.imageUrl);
+        });
     });
-});
+} else {
+    galleryObject = localStorageJSON;   //updating from DOM to Gallery in index.html
+    galleryObject.images.forEach(function (obj) {
+        addNewImage(obj.imageUrl);
+    });
+}
+var galleryObject, output;
 
-function addNewModel(imageUrl, youtubeLink) {
+function addNewImage(imageUrl) {    //adding image to "galleryContainer"
     var containerDiv = document.getElementById("galleryContainer");
-    var firstDiv = document.createElement("div");
-    firstDiv.setAttribute("class", "gallery-image");
-    firstDiv.setAttribute("style", "width: 25%;");
-    containerDiv.appendChild(firstDiv);
-    var movieTrailer = document.createElement("a");
-    movieTrailer.setAttribute("href", youtubeLink);
-    firstDiv.appendChild(movieTrailer);
     var galleryImage = document.createElement("img");
     galleryImage.setAttribute("src", imageUrl);
-    galleryImage.setAttribute("width","200");
-    galleryImage.setAttribute("height","300");
-    var overlayDiv = document.createElement("div");
-    overlayDiv.setAttribute("class", "overlay");
-    overlayDiv.innerHTML = "Click to play the trailer";
-    movieTrailer.appendChild(galleryImage);
-    movieTrailer.appendChild(overlayDiv);
+    galleryImage.setAttribute("class", "image-gallery");
+    containerDiv.appendChild(galleryImage);
 }
